08165b124a86f155f1238a3dcccda7af17ee1476
Lovita Anjani <lovitaanjani@gmail.com>
2021-10-15 18:46:58 +0700

    delete

--- menu/src/main/java/com/example/menu/MenuNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.menu;
-
-class MenuNotFoundException extends RuntimeException{
-
-    MenuNotFoundException(Long id){
-        super("Menu tidak tersedia " + id);
-    }
-    
-}
--- menu/src/main/java/com/example/menu/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.menu;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface MenuRepository extends JpaRepository<Menu, Long>{
-    
-}
